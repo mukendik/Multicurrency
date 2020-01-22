83abdcd9a13990211fc32716a690f3289f4bd0d7
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2020-01-22 11:46:20 +0100

    Unit tests class

---
+++ MoneyExample/MoneyExample/UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyExample
+{
+    [TestClass]
+    class UnitTests
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            
+        }
+    }
+}
