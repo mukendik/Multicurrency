d1ff70865f6ee5295ed0145ff43aa622a5bb8346
MUKENDI KALONJI GHISLAIN <mukendik@gmail.com>
2020-01-22 11:55:36 +0100

    Dollar class add

---
+++ MoneyExample/MoneyExample/Dollar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MoneyExample
+{
+    internal class Dollar
+    {
+        internal double amount;
+        private int v;
+
+        public Dollar(int v)
+        {
+            this.v = v;
+        }
+
+        internal void times(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
